888e3bfcb153e9b77e2f75e64c504d18fbbc86e3
Matheus <matheus.ryan@discente.ufma.br>
2026-04-03 14:49:52 -0300

    add TokenType.java file

---
+++ src/main/java/jackcompiler/TokenType.java
@@ -0,0 +1,60 @@
+package jackcompiler;
+
+/**
+ * Tipos de tokens da linguagem Jack (Nand2Tetris).
+ * Na saída XML, keywords e símbolos mapeiam para as tags {@code keyword} e {@code symbol}.
+ */
+public enum TokenType {
+
+    // Literais e identificador
+    INTEGER_CONSTANT,
+    STRING_CONSTANT,
+    IDENTIFIER,
+
+    // Símbolos (um caractere cada)
+    LBRACE,    // {
+    RBRACE,    // }
+    LPAREN,    // (
+    RPAREN,    // )
+    LBRACKET,  // [
+    RBRACKET,  // ]
+    DOT,       // .
+    COMMA,     // ,
+    SEMICOLON, // ;
+    PLUS,      // +
+    MINUS,     // -
+    STAR,      // *
+    SLASH,     // /
+    AND,       // &
+    PIPE,      // |
+    LT,        // <
+    GT,        // >
+    EQ,        // =
+    TILDE,     // ~
+
+    // Keywords (ordem alfabética auxiliar)
+    KEYWORD_BOOLEAN,
+    KEYWORD_CHAR,
+    KEYWORD_CLASS,
+    KEYWORD_CONSTRUCTOR,
+    KEYWORD_DO,
+    KEYWORD_ELSE,
+    KEYWORD_FALSE,
+    KEYWORD_FIELD,
+    KEYWORD_FUNCTION,
+    KEYWORD_IF,
+    KEYWORD_INT,
+    KEYWORD_LET,
+    KEYWORD_METHOD,
+    KEYWORD_NULL,
+    KEYWORD_RETURN,
+    KEYWORD_STATIC,
+    KEYWORD_THIS,
+    KEYWORD_TRUE,
+    KEYWORD_VAR,
+    KEYWORD_VOID,
+    KEYWORD_WHILE,
+
+    /** Fim do arquivo fonte */
+    EOF
+}
